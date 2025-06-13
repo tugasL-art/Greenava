8ea2170ec6926151dac808052a6744f6d5ffbdd1
tugasL-art <ttugase@gmail.com>
2025-06-14 05:12:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Fungsi sederhana jika kamu mau tambahkan animasi atau interaksi ke depan
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Greenava Website Loaded");
+});
